482431913b7703b363a5eceafe4201b12dd87207
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-06-26 21:22:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob" ;
+
+function upperCaseCustomerName(){
+  customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer(name){
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "hakan";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "nasser";
+  console.Error("there is a type error");
+}
+
+
+
+
