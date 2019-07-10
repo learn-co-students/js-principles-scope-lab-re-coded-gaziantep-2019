3041ac810e6874af1067154e32d6d49c51d9fcfa
kassem241 <34236354+kassem241@users.noreply.github.com>
2019-07-10 13:20:30 +0300

    editing js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob" ;
+
+function upperCaseCustomerName(){
+  customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer(name){
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "rabie";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "nasser";
+  console.Error("there is a type error");
+}
