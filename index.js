dbd3e280c0bf8f2b0c94b3ab62b1f5ef15cf3bfd
Ahmed <a.gado.2df@gmail.com>
2019-07-25 03:16:05 +0300

    Scope Exersice Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName ;
+function upperCaseCustomerName (){
+    customerName = 'BOB'
+}
+var bestCustomer ;
+function bestCustomer (){
+    bestCustomer = 'not bob' ;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'Ahmed'
+}
+const leastFavoriteCustomer = 'Ahmed' ;
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Mido'
+    console.log(leastFavoriteCustomer);
+}
